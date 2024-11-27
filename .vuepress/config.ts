91b706df3f481bca5260f4277284319c66a3c953
lixuan <2789968443@qq.com>
2024-11-27 17:27:39 +0800

    feat: 个人文档编写2024-11-27 17:27:35

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -5,10 +5,12 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import navbarData from "./navbar/index.json";
 // @ts-ignore
 import sidebarData from "./sidebar/index.json";
+import { gitPlugin } from '@vuepress/plugin-git';
 
 const zhNavbar = navbarData;
 const zhSidebar = sidebarData;
 
+// @ts-ignore
 export default defineUserConfig({
   title: "VioletDocs",
   description: "Just playing around",
@@ -22,11 +24,14 @@ export default defineUserConfig({
     authorAvatar: "/avatar.png",
     docsRepo: "https://github.com/Violetlx/violetlx.github.io/tree/gh-pages",
     docsBranch: "gh-pages",
-    docsDir: "example",
+    docsDir: "/docs",
     lastUpdatedText: "",
     // series 为原 sidebar
     series: zhSidebar,
     navbar: zhNavbar,
+    markdown: {
+      lineNumbers: true // 代码块显示行号
+    },
     bulletin: {
       body: [
         {
@@ -36,19 +41,28 @@ export default defineUserConfig({
         },
       ],
     },
-    // commentConfig: {
-    //   type: 'valine',
-    //   // options 与 1.x 的 valineConfig 配置一致
-    //   options: {
-    //     // appId: 'xxx',
-    //     // appKey: 'xxx',
-    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
-    //     // verify: true, // 验证码服务
-    //     // notify: true,
-    //     // recordIP: true,
-    //     // hideComments: true // 隐藏评论
-    //   },
-    // },
+    commentConfig: {
+      type: 'giscus',
+      // options 与 1.x 的 valineConfig 配置一致
+      options: {
+        repo: 'violetlx/violetlx.github.io',
+        repoId: 'R_kgDONUy8uw',
+        category: 'General',
+        categoryId: 'DIC_kwDONUy8u84CknFq',
+        mapping: 'pathname',
+
+        //hideComments: true // 隐藏评论
+      },
+    },
   }),
+  plugins: [
+    gitPlugin({
+      // 配置项
+      // @ts-ignore
+      ignore: [
+          '**/~*.md'
+      ]
+    }),
+  ]
   // debug: true,
 });
